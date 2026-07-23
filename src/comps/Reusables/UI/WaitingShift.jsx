dc2d581e3eb9e1ea8e5cf77b802063b335c6d0c3
shennawy <ahmedshennawy997@gmail.com>
2026-07-23 14:31:34 +0300

    pack

--- src/comps/Reusables/UI/WaitingShift.jsx
+++
@@ -1,8 +0,0 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import WaitingShiftLottie from "../../../assets/lotties/waitingShift.json?url";
-
-function WaitingShift() {
-  return <DotLottieReact src={WaitingShiftLottie} loop autoplay />;
-}
-
-export default WaitingShift;
